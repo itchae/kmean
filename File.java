3fff62fdd816516a534e8c3e11ba39c82745641d
Lucille <lucille.moise@gmail.com>
2018-10-17 16:27:20 +0200

    Auto stash before merge of "master" and "origin/master"

--- File.java
+++ File.java
@@ -7,34 +7,32 @@ public class File {
     public static final String FILENAME = "results.txt";
     public int parseFile(Kmean k,FileReader file){
         int nbPoints;
-		Scanner scanner1 = new Scanner(file);
-		Scanner scanner2 = new Scanner(file);
-		scanner2.nextLine();
-	    while(scanner1.hasNext() && scanner2.hasNext()){
-	    	double value1 = scanner1.nextDouble();
-	    	double value2 = scanner2.nextDouble();
-            k.dataSet.add(new Data(value1,value2));
+        Scanner scanner1 = new Scanner(file);
+        Scanner scanner2 = new Scanner(file);
+        scanner2.nextLine();
+        while (scanner1.hasNext() && scanner2.hasNext()) {
+            double value1 = scanner1.nextDouble();
+            double value2 = scanner2.nextDouble();
+            k.dataSet.add(new Data(value1, value2));
             nbPoints++;
         }
         return nbPoints;
 
-	}
+    }
 
     public void writeInFile(Kmean k) {
         PrintWriter pw = new PrintWriter(FILENAME, "UTF-8");
         String xData = "";
-        String yData ="";
-        String clusterData="";
+        String yData = "";
+        String clusterData = "";
 
         for (Data data : k.dataSet) {
-            xData =+ data.getx() + " ";
-            yData =+ data.gety() + " ";
-            clusterData =+ data.getCluster().getnCluster() + " ";
+            xData = +data.getx() + " ";
+            yData = +data.gety() + " ";
+            clusterData = +data.getCluster().getnCluster() + " ";
         }
 
-        pw.println(xData);
-        pw.println(yData);
-        pw.println(clusterData);
+        pw.print(xData +"\n"+yData+"\n"+clusterData);
         
     }
 }
