a1ec3644560023a827ff8bf30a610771d2068838
Lucille <lucille.moise@gmail.com>
2018-10-17 16:32:55 +0200

    Data + main

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main() {
         Kmean kmean = new Kmean(2);
         File f = new File();
-        kmean.nbPoint = f.parse(kmean, "nomdufichier");
+        kmean.nbPoint = f.parseFile(kmean, new FileReader("data.txt"));
         kmean.initClusters();
         while (kmean.isChanged) {
             kmean.calculClusterPlusProche();
             kmean.calculBarycentre();
         }
-        f.writeInFile(kmean, "lefilereponse");
+        f.writeInFile(kmean);
 
     }
 }
